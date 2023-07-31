45d1bb45581b9049d661acbc1b44b3b7e61449f7
3rooks <Brookslynx@gmail.com>
2023-07-31 19:02:53 -0300

    ✨ Enviroments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Environment } from './constants/environment';
 
 @Module({
-    imports: [],
+    imports: [ConfigModule.forRoot({ isGlobal: true })],
     controllers: [AppController],
     providers: [AppService]
 })
-export class AppModule {}
+export class AppModule {
+    static port: number;
+
+    constructor(private readonly config: ConfigService) {
+        AppModule.port = this.config.get<number>(Environment.PORT);
+    }
+}
